b9ae46c9249e0700d80f984b460a09735ea03f80
Antti Keränen <detegr@gmail.com>
2019-02-25 21:08:06 +0200

    Fix datachannel module that was outputting wrong data due to timing issues

--- digital_video/datachannel.v
+++ digital_video/datachannel.v
@@ -5,14 +5,12 @@ module datachannel(
     input clk,
     input rst,
     input [9:0] data,
-    output dout
+    output reg dout
 );
 
 reg[3:0] i;
 reg[3:0] i_next;
 reg dout_reg;
-
-assign dout = data[i];
 
 always @(*) begin
 	i_next = i + 1;
@@ -22,11 +20,12 @@ always @(*) begin
 end
 
 always @(posedge clk) begin
-	if(rst) begin
-		i <= 4'd9;
-	end else begin
-		i <= i_next;
-	end
+    if(rst) begin
+        i <= 4'd9;
+    end else begin
+        i <= i_next;
+        dout <= data[i];
+    end
 end
 
 endmodule
